82c44dcddf6172422c684fc2077532de22b24045
sugu2344 <suganesh234@gmail.com>
2024-11-17 22:05:25 +0530

    learn width 90% completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
         herogradient2: "#4A16BD",
         featuresbg1: "#D9ECFF",
         featuresbg2: "#F0F3FF",
+        coursesbg1: "#739FE8",
+        coursesbg2: "#4F18CA",
       },
       fontFamily: {
         saira: ["Saira", "sans-serif"],
